4c9306cfb0e8216305ba60e294c2bb7d00e82fc5
Gokhan Nas <n.gokhan36@gmail.com>
2019-01-21 23:26:47 +0300

    Grid Generator, Input Manager

---
+++ Assets/Scripts/Controllers/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    [SerializeField] Transform building;
+    [SerializeField] LayerMask groundLayer;
+
+    private Vector3 target;
+    private Vector3 newBuildingPosition;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            var inputPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(inputPosition, Camera.main.transform.forward, out hit, float.MaxValue, groundLayer))
+            {
+                target = hit.point;
+                MoveBuilding();
+            }
+        }
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(target, 1f);
+        Gizmos.DrawRay(target, Camera.main.transform.forward*1000f);
+    }
+
+    void MoveBuilding()
+    {
+        float gridUnit = 1f;
+        newBuildingPosition.x = Mathf.Floor(target.x / gridUnit) * gridUnit + 1f; // 1f = 2unity building / 2
+        newBuildingPosition.y = 0;
+        newBuildingPosition.z = Mathf.Floor(target.z / gridUnit) * gridUnit + 1f;
+        building.position = newBuildingPosition;
+    }
+}
